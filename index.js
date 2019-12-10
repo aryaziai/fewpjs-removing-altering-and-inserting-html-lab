027946fc2f9d8e17f0f215c04a8f94940161d98f
aryaziai <arya721@gmail.com>
2019-12-09 17:00:26 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+
+let newHeader = document.createElement("h1")
+// created a node
+
+newHeader.id = "victory"; // Set the id
+
+newHeader.innerText = "arya is the champion";
+
+document.body.appendChild(newHeader);
